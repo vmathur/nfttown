18eebdf0d76cd3450c332de57690c382af2c06e9
vmathur <vmathur@users.noreply.github.com>
2023-12-19 08:42:49 -0800

    trying to reduce flicker

--- src/game/Board.js
+++ src/game/Board.js
@@ -81,6 +81,7 @@ export default function Board({charactersRef, ownedCitizens, initialActions, isU
                 return;
             }
             const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
 
             // draw zone map
             Map(ctx, selectedZone);
