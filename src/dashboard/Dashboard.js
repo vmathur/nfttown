13f35f94484c88b4bc9c521ef0148988eeb889a1
vmathur <vmathur@users.noreply.github.com>
2023-12-23 12:20:03 -0800

    move large screen message

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -49,8 +49,8 @@ function Dashboard({charactersRef, account, ownedCitizens, clickInfoHandler, set
     return (
       <div className="citizen-container">
         {!account && charactersRef.current.length<maxCitizens ? <div className="login-message"><i>Connect wallet</i></div> : ''}
-        <div className="dashboard-mobile-message"><i>View on a larger display for a better experience</i></div>
         {canMint && account && charactersRef.current.length<maxCitizens ? <button onClick={callMint} className="mint-button button-primary">+ Mint a citizen</button> : ''}
+        <div className="dashboard-mobile-message"><i>View on a larger display for a better experience</i></div>
         <div className="citien-section-container">{allSections}</div>
       </div>
     );
